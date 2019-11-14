2dde69fd079d1db96edc646a731edb8b7ceb4f02
dbarg <derek.barge@fysik.su.su>
2019-11-13 21:06:00 -0600

    work in progress

--- merge_pax.py
+++ merge_pax.py
@@ -23,6 +23,57 @@ import merge_process_event as process_event
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 
+def fill_sarr(strArr2, df, idx_arr):
+
+    return 
+
+    #df = utils_fax.nsToSamples(df, 't_first_electron_true')
+    #df = utils_fax.nsToSamples(df, 't_last_electron_true')
+    #df = utils_fax.nsToSamples(df, 't_first_photon_true')
+    #df = utils_fax.nsToSamples(df, 't_last_photon_true')
+    #df = utils_fax.nsToSamples(df, 's2_center_time')
+        
+        
+    #assert(df is not None)
+    #
+    #idx_df = idx_arr 
+    #
+    #try:
+    #    strArr2[idx_arr]['true_left']    = df.at[idx_df, 't_first_electron_true']
+    #    strArr2[idx_arr]['true_right']   = df.at[idx_df, 't_last_photon_true']
+    #    strArr2[idx_arr]['true_nels']    = df.at[idx_df, 'n_electrons_true']
+    #    strArr2[idx_arr]['true_nphs']    = df.at[idx_df, 'n_photons_true']
+    #    strArr2[idx_arr]['x_ins']        = df.at[idx_df, 'x_ins']
+    #    strArr2[idx_arr]['y_ins']        = df.at[idx_df, 'y_ins']            
+    #    strArr2[idx_arr]['x_true']       = df.at[idx_df, 'x_true']
+    #    strArr2[idx_arr]['y_true']       = df.at[idx_df, 'y_true']
+    #except Exception as e:
+    #    print("Exception Filling Truth!")
+    #    print(e)
+    #    
+    #try:
+    #    strArr2[idx_arr]['x_s2']         = df.at[idx_df, 's2_x']
+    #    strArr2[idx_arr]['y_s2']         = df.at[idx_df, 's2_y']
+    #    strArr2[idx_arr]['s2_left']      = df.at[idx_df, 's2_left']
+    #    strArr2[idx_arr]['s2_right']     = 2*(df.at[idx_df, 's2_center_time' ] - df.at[idx_df, 's2_left' ])
+    #    strArr2[idx_arr]['s2_area']      = df.at[idx_df, 's2_area']
+    #except Exception as e:
+    #    print("Exception Filling Reco!")
+    #    print(e)
+    #    
+#
+    #    #strArr2[idx_arr]['index_left']   = min(strArr2[idx_arr]['true_left'] , strArr2[idx_arr]['s2_left'] )
+    #    ##strArr2[idx_arr]['s2_truncated'] = num_s2_samples_truncated
+    #    #strArr2[idx_arr]['s2_area']      = df.at[idx_df, 's2_area']
+    #    #strArr2[idx_arr]['image']        = arr2d_s2
+    #    #strArr2[idx_arr]['s2_areas']     = arr_s2_areas
+    #  
+    #    
+    #return
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
 class mergePax():
 
     #--------------------------------------------------------------------------
@@ -44,10 +95,35 @@ class mergePax():
         cfg           = utils_event.getConfig(jsonfilename)
         zfile         = zipfile.ZipFile(zipname, 'r')
         zip_namelist  = zfile.namelist()
-        n_zip         = len(zip_namelist)
+        n_pkl_per_zip = len(zip_namelist)
         df_merged     = pd.DataFrame()
             
-        assert(n_zip == 1000)
+        assert(n_pkl_per_zip == 1000)
+       
+        strArr = np.zeros(
+            n_pkl_per_zip,
+            dtype=[
+                ('x_ins'       , np.float32), # Truth
+                ('y_ins'       , np.float32),
+                ('true_left'   , np.int32),
+                ('true_right'  , np.int32),
+                ('true_x'      , np.float32),
+                ('true_y'      , np.float32),
+                ('true_n_els'  , np.int32),
+                ('true_n_phs'  , np.int32),
+                ('left_index'  , np.int32),   # Reco
+                ('left'        , np.int32),
+                ('right'       , np.int32),
+                ('x'           , np.float32),
+                ('y'           , np.float32),
+                ('z'           , np.float32),
+                ('dt'          , np.float32),
+                ('s2_area'     , np.float32),
+                ('s2_areas'    , np.float16, 127),
+                ('image'       , np.float16, (127, 1000) ),
+                #('s2_truncated', np.int32),
+            ]
+        )
         
         
         #----------------------------------------------------------------------
@@ -107,10 +183,20 @@ class mergePax():
             
             
         #----------------------------------------------------------------------
+        # Save
         #----------------------------------------------------------------------
             
         df_merged.reset_index(inplace=True, drop=True)
         df_merged.to_pickle('df_merge.pkl')
+            
+        #f_out = dir_out + '/strArr_dir{0}'.format(i_dir)
+        f_out = './strArr_dir{0}'.format(i_zip)
+        print("\nOutfile: {0}".format(f_out))
+        np.save(f_out, strArr)
+        
+
+        #----------------------------------------------------------------------
+        #----------------------------------------------------------------------
         
         return
 
