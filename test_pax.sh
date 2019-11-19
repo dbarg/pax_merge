28aabf73e4f211886e3fddea94c18fab2fac7409
dbarg <derek.barge@fysik.su.su>
2019-11-19 14:12:39 -0600

    work in progress

--- test_pax.sh
+++ test_pax.sh
@@ -16,5 +16,5 @@
 cd ${HOME}/dali/xe1t-processing/pax_merge
 source ~/.bash/.setup_pax_head.sh
 
-#python ./merge_pax.py -dir_out ./temp_pax -dir_in ../../data-xe1t/pax2019-11-14/zip/ -dir_fmt instructions_[0-9]* -zip_fmt *.zip -n_intr 1 -isStrict True
-python ./merge_pax.py -dir_out ./temp_pax -dir_in ../../data-xe1t/pax2019-11-14/zip/ -dir_fmt instructions_00000[0-2] -zip_fmt *.zip -n_intr 1 -isStrict True
+#python ./merge_pax.py -dir_out ./temp_pax -dir_in ../../data-xe1t/pax2019-11-14/zip/ -dir_fmt instructions_00000[0-2] -zip_fmt *.zip -n_intr 1 -isStrict True
+python ./merge_pax.py -dir_out ./temp_pax -dir_in ../../data-xe1t/pax2019-11-14/zip/ -dir_fmt instructions_[0-9]* -zip_fmt *.zip -n_intr 1 -isStrict True
